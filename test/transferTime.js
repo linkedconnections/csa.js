2ef1129a1e2047559fca3d30c34ae296998e3c9a
Dylan Van Assche <dylan.van.assche@protonmail.com>
2018-09-19 19:38:53 +0200

    Trying to fix Mocha tests

--- test/transferTime.js
+++ test/transferTime.js
@@ -11,67 +11,71 @@ describe('Test minimum transfer time', function () {
     var queries = [
         {
             //Ghent -> Spa
-            departureStop : "8892007",
-            departureTime : new Date("2015-10-01T12:00:00.000Z"),
+            departureStop: "8892007",
+            departureTime: new Date("2015-10-01T12:00:00.000Z"),
             "minimumTransferTime": minimumTransferTime,
-            arrivalStop : "8844404"
+            arrivalStop: "8844404"
         },
         {
             //Antwerp -> Brussels
-            departureStop : "8821006",
-            departureTime : new Date("2015-10-01T12:00:00.000Z"),
+            departureStop: "8821006",
+            departureTime: new Date("2015-10-01T12:00:00.000Z"),
             "minimumTransferTime": minimumTransferTime,
-            arrivalStop : "8813003"
+            arrivalStop: "8813003"
         },
         {
             //Lichtervel -> Ghent
-            departureStop : "8892205",
-            departureTime : new Date("2015-10-01T12:00:00.000Z"),
+            departureStop: "8892205",
+            departureTime: new Date("2015-10-01T12:00:00.000Z"),
             "minimumTransferTime": minimumTransferTime,
-            arrivalStop : "8892007"
+            arrivalStop: "8892007"
         }
     ];
     async.eachSeries(queries, function (query, doneEntry) {
         //let's create our route planner
-        var planner = new Planner(query);
-        var ending = "";
-        if (query.arrivalStop) {
-            ending += " to " + query.arrivalStop;
-        }
-        describe(query.departureStop + ending, function () {
-            var readStream = fs.createReadStream('test/data/nmbs20151001_20151002.json.gz', {flags: 'r'});
-            var result = readStream.pipe(zlib.createGunzip()).pipe(new Deserialize()).pipe(planner);
-            it("should yield a result and respect minimum transfertimes", function (done) {
-                var mst = {};
-                result.on("data", function (data) {
-                    //It should never give two times the same arrivalStop!
-                    if (mst[data["departureStop"]]) {
-                        if(!(mst[data["departureStop"]]["gtfs:trip"] == data["gtfs:trip"])) {
-                            var transferTime = (data["arrivalTime"].getTime() - mst[data["departureStop"]]["arrivalTime"].getTime())/1000;
-                            if(transferTime < minimumTransferTime) {
-                                done('Minimum transfertime exceeded at transfer: ' + mst[data["departureStop"]].departureStop + " -> " + data.departureStop + " - " + transferTime);
+        let walkingSpeed = 5.0;
+        let stopsData = [];
+        new Planner(query, undefined, walkingSpeed, stopsData).then((instance) => {
+            var planner = instance;
+            var ending = "";
+            if (query.arrivalStop) {
+                ending += " to " + query.arrivalStop;
+            }
+            describe(query.departureStop + ending, function () {
+                var readStream = fs.createReadStream('test/data/nmbs20151001_20151002.json.gz', { flags: 'r' });
+                var result = readStream.pipe(zlib.createGunzip()).pipe(new Deserialize()).pipe(planner);
+                it("should yield a result and respect minimum transfertimes", function (done) {
+                    var mst = {};
+                    result.on("data", function (data) {
+                        //It should never give two times the same arrivalStop!
+                        if (mst[data["departureStop"]]) {
+                            if (!(mst[data["departureStop"]]["gtfs:trip"] == data["gtfs:trip"])) {
+                                var transferTime = (data["arrivalTime"].getTime() - mst[data["departureStop"]]["arrivalTime"].getTime()) / 1000;
+                                if (transferTime < minimumTransferTime) {
+                                    done('Minimum transfertime exceeded at transfer: ' + mst[data["departureStop"]].departureStop + " -> " + data.departureStop + " - " + transferTime);
+                                }
                             }
                         }
-                    }
-                    mst[data.arrivalStop] = data;
-                });
-                result.on("result", function (path) {
-                    done();
-                    doneEntry();
-                    readStream.destroy();
-                    result.destroy();
-                });
-                result.on("error", function (error) {
-                    done("error encountered" + error);
-                    doneEntry();
-                });
-                result.on("end", function () {
-                    if (query.arrivalStop) {
-                        done("no path found");
-                    } else {
+                        mst[data.arrivalStop] = data;
+                    });
+                    result.on("result", function (path) {
                         done();
-                    }
-                    doneEntry();
+                        doneEntry();
+                        readStream.destroy();
+                        result.destroy();
+                    });
+                    result.on("error", function (error) {
+                        done("error encountered" + error);
+                        doneEntry();
+                    });
+                    result.on("end", function () {
+                        if (query.arrivalStop) {
+                            done("no path found");
+                        } else {
+                            done();
+                        }
+                        doneEntry();
+                    });
                 });
             });
         });
@@ -82,21 +86,21 @@ describe('Test transfer time fetcher', function () {
     var queries = [
         {
             //Ghent -> Spa
-            departureStop : "8892007",
-            departureTime : new Date("2015-10-01T12:00:00.000Z"),
-            arrivalStop : "8844404"
+            departureStop: "8892007",
+            departureTime: new Date("2015-10-01T12:00:00.000Z"),
+            arrivalStop: "8844404"
         },
         {
             //Antwerp -> Brussels
-            departureStop : "8821006",
-            departureTime : new Date("2015-10-01T12:00:00.000Z"),
-            arrivalStop : "8813003"
+            departureStop: "8821006",
+            departureTime: new Date("2015-10-01T12:00:00.000Z"),
+            arrivalStop: "8813003"
         },
         {
             //Lichtervel -> Ghent
-            departureStop : "8892205",
-            departureTime : new Date("2015-10-01T12:00:00.000Z"),
-            arrivalStop : "8892007"
+            departureStop: "8892205",
+            departureTime: new Date("2015-10-01T12:00:00.000Z"),
+            arrivalStop: "8892007"
         }
     ];
     var transferTimes = {
@@ -111,7 +115,7 @@ describe('Test transfer time fetcher', function () {
         }
         return transferTime;
     }
-    var transferTimesFetcher = function() {};
+    var transferTimesFetcher = function () { };
     transferTimesFetcher.get = function (previousConnection, connection) {
         return new Promise(function (fulfill) {
             fulfill(getTransferTimes(previousConnection, connection))
@@ -119,45 +123,49 @@ describe('Test transfer time fetcher', function () {
     };
     async.eachSeries(queries, function (query, doneEntry) {
         //let's create our route planner
-        var planner = new Planner(query,transferTimesFetcher);
-        var ending = "";
-        if (query.arrivalStop) {
-            ending += " to " + query.arrivalStop;
-        }
-        describe(query.departureStop + ending, function () {
-            var readStream = fs.createReadStream('test/data/nmbs20151001_20151002.json.gz', {flags: 'r'});
-            var result = readStream.pipe(zlib.createGunzip()).pipe(new Deserialize()).pipe(planner);
-            it("should yield a result and respect transfertimes from fetcher", function (done) {
-                var mst = {};
-                result.on("data", function (data) {
-                    //It should never give two times the same arrivalStop!
-                    if (mst[data["departureStop"]]) {
-                        if(!(mst[data["departureStop"]]["gtfs:trip"] == data["gtfs:trip"])) {
-                            var transferTime = (data["arrivalTime"].getTime() - mst[data["departureStop"]]["arrivalTime"].getTime())/1000;
-                            if(transferTime < getTransferTimes(mst[data["departureStop"]],data)) {
-                                 done('Minimum transfertime exceeded at transfer: ' + mst[data["departureStop"]].departureStop + " -> " + data.departureStop + " - " + transferTime);
+        let walkingSpeed = 5.0;
+        let stopsData = [];
+        new Planner(query, transferTimesFetcher, walkingSpeed, stopsData).then((instance) => {
+            var planner = instance;
+            var ending = "";
+            if (query.arrivalStop) {
+                ending += " to " + query.arrivalStop;
+            }
+            describe(query.departureStop + ending, function () {
+                var readStream = fs.createReadStream('test/data/nmbs20151001_20151002.json.gz', { flags: 'r' });
+                var result = readStream.pipe(zlib.createGunzip()).pipe(new Deserialize()).pipe(planner);
+                it("should yield a result and respect transfertimes from fetcher", function (done) {
+                    var mst = {};
+                    result.on("data", function (data) {
+                        //It should never give two times the same arrivalStop!
+                        if (mst[data["departureStop"]]) {
+                            if (!(mst[data["departureStop"]]["gtfs:trip"] == data["gtfs:trip"])) {
+                                var transferTime = (data["arrivalTime"].getTime() - mst[data["departureStop"]]["arrivalTime"].getTime()) / 1000;
+                                if (transferTime < getTransferTimes(mst[data["departureStop"]], data)) {
+                                    done('Minimum transfertime exceeded at transfer: ' + mst[data["departureStop"]].departureStop + " -> " + data.departureStop + " - " + transferTime);
+                                }
                             }
                         }
-                    }
-                    mst[data.arrivalStop] = data;
-                });
-                result.on("result", function (path) {
-                    done();
-                    doneEntry();
-                    readStream.destroy();
-                    result.destroy();
-                });
-                result.on("error", function (error) {
-                    done("error encountered" + error);
-                    doneEntry();
-                });
-                result.on("end", function () {
-                    if (query.arrivalStop) {
-                        done("no path found");
-                    } else {
+                        mst[data.arrivalStop] = data;
+                    });
+                    result.on("result", function (path) {
                         done();
-                    }
-                    doneEntry();
+                        doneEntry();
+                        readStream.destroy();
+                        result.destroy();
+                    });
+                    result.on("error", function (error) {
+                        done("error encountered" + error);
+                        doneEntry();
+                    });
+                    result.on("end", function () {
+                        if (query.arrivalStop) {
+                            done("no path found");
+                        } else {
+                            done();
+                        }
+                        doneEntry();
+                    });
                 });
             });
         });
